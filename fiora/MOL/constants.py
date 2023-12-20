0ea1bb2ca499cbd9f4d86b78af6f102314415175
Yannek Nowatzky <ch4perone@gmail.com>
2023-12-20 12:17:58 +0100

    Update hydrogen masses in constants.py

--- fiora/MOL/constants.py
+++ fiora/MOL/constants.py
@@ -1,31 +1,65 @@
+# ADDUCT_WEIGHTS = {
+#     "[M+H]+": 1.007276,
+#     "[M+NH4]+": 18.033823,
+#     "[M+Na]+": 22.989218 ,
+#     "[M-H]-": -1.007276,
+    
+#     #
+#     # positvie fragment rearrangements
+#     #
+#     "[M-H]+": -1.007276, # Double bond replacing 2 hydrogen atoms + H
+#     "[M-3H]+": -1.007276 * 3, # 2 Double bonds  + H
+#     # experimental cases
+#     "[M]+": 0,
+#     "[M-2H]+": -1.007276 * 2,
+#     "[M-4H]+": -1.007276 * 4,
+#     "[M-5H]+": -1.007276 * 5,
+    
+    
+#     #
+#     # negative fragment rearrangements
+#     # 
+    
+#     "[M]-": 0, # can this even exist?
+#     "[M-2H]-": -1.007276 * 2,
+#     "[M-3H]-": -1.007276 * 3,
+#     "[M-4H]-": -1.007276 * 4
+    
+#     }
+import rdkit.Chem as Chem
+
+h_minus = Chem.MolFromSmiles("[H-]") #hydrid
+h_plus = Chem.MolFromSmiles("[H+]") #h proton
+h_2 = Chem.MolFromSmiles("[HH]") #h2 
+
 ADDUCT_WEIGHTS = {
-    "[M+H]+": 1.007276,
+    "[M+H]+": Chem.Descriptors.ExactMolWt(h_plus), #1.007276,
     "[M+NH4]+": 18.033823,
     "[M+Na]+": 22.989218 ,
-    "[M-H]-": -1.007276,
+    "[M-H]-": -1*Chem.Descriptors.ExactMolWt(h_plus),
     
     #
     # positvie fragment rearrangements
     #
-    "[M-H]+": -1.007276, # Double bond replacing 2 hydrogen atoms + H
-    "[M-3H]+": -1.007276 * 3, # 2 Double bonds  + H
-    # experimental cases
+    "[M-H]+": -1*Chem.Descriptors.ExactMolWt(h_minus), # Double bond replacing 2 hydrogen atoms + H
     "[M]+": 0,
-    "[M-2H]+": -1.007276 * 2,
-    "[M-4H]+": -1.007276 * 4,
-    "[M-5H]+": -1.007276 * 5,
+    "[M-2H]+": -1 * Chem.Descriptors.ExactMolWt(h_2), # Loosing proton and hydrid
+    "[M-3H]+": -1 * Chem.Descriptors.ExactMolWt(h_2) - 1 * Chem.Descriptors.ExactMolWt(h_minus), # 2 Double bonds  + H
+    # experimental cases
+    #"[M-4H]+": -1.007276 * 4,
+    #"[M-5H]+": -1.007276 * 5,
     
     
     #
     # negative fragment rearrangements
     # 
     
-    "[M]-": 0, # can this even exist?
-    "[M-2H]-": -1.007276 * 2,
-    "[M-3H]-": -1.007276 * 3,
-    "[M-4H]-": -1.007276 * 4
-    
+    # "[M-H]-": -1*Chem.Descriptors.ExactMolWt(h_plus), # see above
+    "[M]-": 0, # could be one electron too many 
+    "[M-2H]-": -1 * Chem.Descriptors.ExactMolWt(h_2),
+    "[M-3H]-": -1 * Chem.Descriptors.ExactMolWt(h_2) - 1 * Chem.Descriptors.ExactMolWt(h_plus),    
     }
+
 
 
 PPM = 1/1000000
