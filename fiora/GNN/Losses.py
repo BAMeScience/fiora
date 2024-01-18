9b0ac529b5e4bf55ef097be47908e7c9a571647b
Yannek Nowatzky <ch4perone@gmail.com>
2024-01-18 13:28:34 +0100

    Add WeightedMSEMetric

--- fiora/GNN/Losses.py
+++ fiora/GNN/Losses.py
@@ -1,4 +1,8 @@
 import torch
+from torch import Tensor
+from torchmetrics import Metric
+from torchmetrics.utilities import dim_zero_cat
+
 
 class WeightedMSE(torch.nn.Module):
     def __init__(self):
@@ -6,4 +10,23 @@ class WeightedMSE(torch.nn.Module):
 
     def forward(self, input, target, weight):
         loss = (weight * (input - target) ** 2)
+        return loss.mean()
+
+class WeightedMSEMetric(Metric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_state("preds", default=[], dist_reduce_fx="cat")
+        self.add_state("target", default=[], dist_reduce_fx="cat")
+        self.add_state("weights", default=[], dist_reduce_fx="cat")
+
+    def update(self, preds: Tensor, target: Tensor, weight: Tensor) -> None:
+        self.preds.append(preds)
+        self.target.append(target)
+        self.weights.append(weight)
+
+    def compute(self) -> Tensor:
+        preds = dim_zero_cat(self.preds)
+        target = dim_zero_cat(self.target)
+        weights = dim_zero_cat(self.weights)
+        loss = (weights * (preds - target) ** 2)
         return loss.mean()
