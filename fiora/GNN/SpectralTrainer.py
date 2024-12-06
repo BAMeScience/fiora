0a5c7ec223ecbb9007c5ae2d93e60d84316ef39a
Yannek Nowatzky <ch4perone@gmail.com>
2024-12-06 11:38:20 +0100

    Clean up trainer classes and checkpoint system | Tested in debug mode

--- fiora/GNN/SpectralTrainer.py
+++ fiora/GNN/SpectralTrainer.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader, Dataset
 import torch_geometric.loader as geom_loader
 from torchmetrics import Accuracy, MetricTracker, MetricCollection, Precision, Recall, PrecisionRecallCurve, MeanSquaredError, MeanAbsoluteError, R2Score
 from sklearn.model_selection import train_test_split
-from typing import Literal, List, Callable, Any
+from typing import Literal, List, Callable, Any, Dict
 
 from fiora.GNN.Trainer import Trainer
 from fiora.GNN.Datasets import collate_graph_batch, collate_graph_edge_batch
@@ -15,7 +15,7 @@ GNN Trainer
 '''
 
 class SpectralTrainer(Trainer):
-    def __init__(self, data, train_val_split: float= 0.8, split_by_group: bool=False, only_training: bool=False, train_keys: List[int]=[], val_keys: List[int]=[], y_tag: str="y", metric_dict=None, problem_type: Literal["classification", "regression", "softmax_regression"]="classification", library: Literal["standard", "geometric"]="geometric", num_workers: int=0, seed: int=42, device: str="cpu"):
+    def __init__(self, data: Any, train_val_split: float= 0.8, split_by_group: bool=False, only_training: bool=False, train_keys: List[int]=[], val_keys: List[int]=[], y_tag: str="y", metric_dict: Dict=None, problem_type: Literal["classification", "regression", "softmax_regression"]="classification", library: Literal["standard", "geometric"]="geometric", num_workers: int=0, seed: int=42, device: str="cpu"):
         
         super().__init__(data, train_val_split, split_by_group, only_training, train_keys, val_keys, seed, num_workers, device)
         self.y_tag = y_tag
@@ -30,22 +30,10 @@ class SpectralTrainer(Trainer):
                 for data_split in ["train", "val", "masked_val", "test"]
             }
         else:
-            self.metrics = _get_default_metrics(problem_type)
+            self.metrics = self._get_default_metrics(problem_type)
         self.loader_base = geom_loader.DataLoader if library == "geometric" else DataLoader
-        
-        # Set output transform according to spectral output format
-        self.output_transform = lambda y, batch: y #equivalent to torch.nn.Identity()
-        if problem_type == "softmax_regression":
-            self.softmax = torch.nn.Softmax(dim=0)
-            self.output_transform = self.graph_based_softmax_regression
     
-    def graph_based_softmax_regression(self, y, batch):      
-        edge_graph_map = batch["batch"][batch["edge_index"][0,:]]
-        for i in range(batch.num_graphs):
-            y[edge_graph_map == i] = self.softmax(y[edge_graph_map == i])
-        return 2. * y # times 2, since edges are undirected and therefore doubled
-    
-    def training_loop(self, model, dataloader, optimizer, loss_fn, metrics, with_weights=False, with_RT=False, with_CCS=False, rt_metric=False, title=""):
+    def _training_loop(self, model, dataloader, optimizer, loss_fn, metrics, with_weights=False, with_RT=False, with_CCS=False, rt_metric=False, title=""):
         training_loss = 0
         metrics.increment()       
 
@@ -58,7 +46,8 @@ class SpectralTrainer(Trainer):
             kwargs={}
             if with_weights:
                 kwargs={"weight": batch["weight_tensor"]}
-             
+            
+            # Compute loss
             loss = loss_fn(y_pred["fragment_probs"], batch[self.y_tag], **kwargs) # with logits
             if not rt_metric: metrics(y_pred["fragment_probs"], batch[self.y_tag], **kwargs) # call update
 
@@ -78,16 +67,14 @@ class SpectralTrainer(Trainer):
             if rt_metric:
                 metrics(y_pred["rt"][batch["retention_mask"]], batch["retention_time"][batch["retention_mask"]], **kwargs) # call update
                 metrics(y_pred["ccs"][batch["ccs_mask"]], batch["ccs"][batch["ccs_mask"]], **kwargs) # call update
-                
                     
             # Backpropagate
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
             
 
-        # On epoch end: Evaluation
+        # End of training cycle: Evaluation
         stats = metrics.compute()
         training_loss /= len(dataloader)
         
@@ -98,14 +85,12 @@ class SpectralTrainer(Trainer):
         return stats
         
 
-    def validation_loop(self, model, dataloader, loss_fn, metrics, with_weights=False, with_RT=False,  with_CCS=False, rt_metric=False,  mask_name=None, title="Validation"):
+    def _validation_loop(self, model, dataloader, loss_fn, metrics, with_weights=False, with_RT=False,  with_CCS=False, rt_metric=False,  mask_name=None, title="Validation"):
         metrics.increment()
         with torch.no_grad():
             for id, batch in enumerate(dataloader):
                     model.eval()
                     y_pred = model(batch, with_RT=with_RT, with_CCS=with_CCS)
-                    #y_pred = self.output_transform(y_pred, batch) # Transform output in case of softmax_regression
-
                     if mask_name:
                         loss = loss_fn(y_pred["fragment_probs"][batch[mask_name]], batch[self.y_tag][batch[mask_name]])
                         metrics.update(y_pred["fragment_probs"][batch[mask_name]], batch[self.y_tag][batch[mask_name]])
@@ -118,20 +103,22 @@ class SpectralTrainer(Trainer):
                             metrics.update(y_pred["fragment_probs"], batch[self.y_tag], **kwargs)
                         if rt_metric:
                             metrics(y_pred["rt"][batch["retention_mask"]], batch["retention_time"][batch["retention_mask"]], **kwargs) # call update
-                            metrics(y_pred["ccs"][batch["ccs_mask"]], batch["ccs"][batch["ccs_mask"]], **kwargs) # call update
-                        
+                            metrics(y_pred["ccs"][batch["ccs_mask"]], batch["ccs"][batch["ccs_mask"]], **kwargs) # call update                      
 
         # End of Validation cycle
         stats = metrics.compute()
-        print(f'\t{title} RMSE: {torch.sqrt(stats["mse"]):>.4f}') #MSE: {stats["mse"]:>.3f}; MAE: {stats["mae"]:>.3f}
+        print(f'\t{title} RMSE: {torch.sqrt(stats["mse"]):>.4f}')
         return stats
         
         
     # Training function
-    def train(self, model, optimizer, loss_fn, scheduler=None, batch_size=1, epochs=2, val_every_n_epochs=1, masked_validation=False, with_RT=True, with_CCS=True, rt_metric=False, mask_name="validation_mask", tag=""):
+    def train(self, model, optimizer, loss_fn, scheduler=None, batch_size=16, epochs=2, val_every_n_epochs=1, use_validation_mask=False, with_RT=True, with_CCS=True, rt_metric=False, mask_name="validation_mask", tag="") -> Dict[str, Any]:
         
-        checkpoint_stats = {"epoch": -1, "val_loss": 100000.0, "file": f"../../checkpoint_{tag}.best.pt"}
+        # Set up checkpoint system and model info
+        self._init_checkpoint_system(save_path=f"../../checkpoint_{tag}.best.pt")
         model.model_params["training_label"] = self.y_tag
+        
+        # Stage data into dataloader
         training_loader = self.loader_base(self.training_data, batch_size=batch_size, num_workers=self.num_workers, shuffle=True)
         if not self.only_training:
             validation_loader = self.loader_base(self.validation_data, batch_size=batch_size, num_workers=self.num_workers, shuffle=True)
@@ -139,26 +126,28 @@ class SpectralTrainer(Trainer):
         
         # Main loop
         for e in range(epochs):
-            self.training_loop(model, training_loader, optimizer, loss_fn, self.metrics["train"], title=f'Epoch {e + 1}/{epochs}: ', with_weights=using_weighted_loss_func, with_RT=with_RT, with_CCS=with_CCS, rt_metric=rt_metric)
+            
+            # Training
+            self._training_loop(model, training_loader, optimizer, loss_fn, self.metrics["train"], title=f'Epoch {e + 1}/{epochs}: ', with_weights=using_weighted_loss_func, with_RT=with_RT, with_CCS=with_CCS, rt_metric=rt_metric)
+            
+            # Validation
             is_val_cycle = not self.only_training and ((e + 1) % val_every_n_epochs == 0)
-            if is_val_cycle:
-                if masked_validation:
-                    val_stats = self.validation_loop(model, validation_loader, loss_fn, self.metrics["masked_val"],  with_weights=using_weighted_loss_func, with_RT=with_RT, with_CCS=with_CCS, mask_name=mask_name, title="Masked Val.", rt_metric=rt_metric)
-                else:
-                    val_stats = self.validation_loop(model, validation_loader, loss_fn, self.metrics["val"], with_weights=using_weighted_loss_func, with_RT=with_RT, with_CCS=with_CCS, rt_metric=rt_metric)
-                if val_stats["mse"].tolist() < checkpoint_stats["val_loss"]:
-                    checkpoint_stats["epoch"] = e+1
+            if is_val_cycle:   
+                val_stats = self._validation_loop(model, validation_loader, loss_fn, self.metrics["masked_val"] if use_validation_mask else self.metrics["val"], with_weights=using_weighted_loss_func, with_RT=with_RT, with_CCS=with_CCS, rt_metric=rt_metric, mask_name=mask_name if use_validation_mask else None, title="Masked Validation" if use_validation_mask else "Validation")
+                
+                # Update checkpoint
+                if val_stats["mse"].tolist() < self.checkpoint_stats["val_loss"]:
+                    self._update_checkpoint({"epoch": e+1, "val_loss": val_stats["mse"].tolist()}, model)
                     print(f"\t >> Set new checkpoint to epoch {e+1}")
-                    checkpoint_stats["val_loss"] = val_stats["mse"].tolist()
-                    model.save(checkpoint_stats["file"])
+            
+            # End of epoch: Advance scheduler
             if scheduler:
                 if isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
-                    # step only after validation
                     if is_val_cycle:
                         scheduler.step(torch.sqrt(val_stats["mse"]))
                 else:
                     scheduler.step()
-            
+                    
         print("Finished Training!")
-        return checkpoint_stats
+        return self.checkpoint_stats
     
--- fiora/GNN/Trainer.py
+++ fiora/GNN/Trainer.py
@@ -4,11 +4,11 @@ import numpy as np
 from torch.utils.data import DataLoader, Dataset
 from torchmetrics import Accuracy, MetricTracker, MetricCollection, Precision, Recall, PrecisionRecallCurve, MeanSquaredError, MeanAbsoluteError, R2Score
 from sklearn.model_selection import train_test_split
-from typing import Literal, List
+from typing import Literal, List, Dict, Any
 
 
 class Trainer(ABC):
-    def __init__(self, data, train_val_split: float=0.8, split_by_group: bool=False, only_training: bool=False,
+    def __init__(self, data: Any, train_val_split: float=0.8, split_by_group: bool=False, only_training: bool=False,
                  train_keys: List[int]=[], val_keys: List[int]=[], seed: int=42, num_workers: int=0, device: str="cpu") -> None:
         
         self.only_training = only_training
@@ -60,6 +60,16 @@ class Trainer(ABC):
         
         return metrics
     
+    def _init_checkpoint_system(self, save_path: str) -> None:
+        self.checkpoint_stats = {
+            "epoch": -1,
+            "val_loss": 100000.0,
+            "file": save_path}
+    
+    def _update_checkpoint(self, new_checkpoint_data: Dict[str, Any], model, save_checkpoint: bool=True) -> None:
+        self.checkpoint_stats.update(new_checkpoint_data)
+        model.save(self.checkpoint_stats["file"])
+
     
     def is_group_in_training_set(self, group_id):
         return (group_id in self.train_keys)
@@ -67,16 +77,12 @@ class Trainer(ABC):
     def is_group_in_validation_set(self, group_id):
         return (group_id in self.val_keys)
 
-    def get_loader(self, dataset, batch_size, shuffle=True):
-        return DataLoader(dataset, batch_size=batch_size, num_workers=self.num_workers, shuffle=shuffle)
-
-
     @abstractmethod
-    def training_loop(self, model, dataloader, optimizer, loss_fn, **kwargs):
+    def _training_loop(self, model, dataloader, optimizer, loss_fn, **kwargs):
         pass
 
     @abstractmethod
-    def validation_loop(self, model, dataloader, loss_fn, **kwargs):
+    def _validation_loop(self, model, dataloader, loss_fn, **kwargs):
         pass
 
     @abstractmethod
