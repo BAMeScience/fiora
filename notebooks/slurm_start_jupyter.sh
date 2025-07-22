1ead47b97e720675f282c338d6de3946b75ef8aa
ynowatzk <ch4perone@gmail.com>
2025-07-22 10:10:27 +0200

    Adjust slurm cmd line instructions

--- notebooks/slurm_start_jupyter.sh
+++ notebooks/slurm_start_jupyter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Request resources and start an interactive session
-srun --gres=shard:1 --qos=interactive --mem=200G --time=72:00:00 --pty bash
+# srun --gres=shard:1 --qos=interactive --mem=200G --time=72:00:00 --pty bash
+srun --gres=gpu:4g.40gb:1 --qos=interactive --time=72:00:00 --pty bash
 # --gres=1 --qos=normal
 
 # Activate the conda environment
@@ -9,3 +10,13 @@ conda activate fiora
 
 # Start Jupyter Notebook
 jupyter notebook --no-browser --port=8888 --ip=127.0.0.1
+
+# Previously used script (commented out for reference)
+# #!/bin/bash
+# #SBATCH --gpus=1
+# #SBATCH --time=02:00:00
+# #SBATCH --mem=16G
+# #SBATCH --output=jupyter-%J.oute
+
+# conda activate fiora
+# jupyter notebook --no-browser --port=8888 --ip=127.0.0.1
