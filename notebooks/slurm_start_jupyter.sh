8952dd9b1ebc5b79b52778594b1dec828a9a3cc9
ynowatzk <ch4perone@gmail.com>
2025-10-15 12:51:55 +0200

    Update slurm cmd

--- notebooks/slurm_start_jupyter.sh
+++ notebooks/slurm_start_jupyter.sh
@@ -3,8 +3,9 @@
 # Request resources and start an interactive session
 # srun --gres=shard:1 --qos=interactive --mem=200G --time=72:00:00 --pty bash
 # srun --gres=gpu:4g.40gb:1 --qos=interactive --time=72:00:00 --pty bash
-srun --gres=gpu:1 --qos=interactive --time=72:00:00 --pty bash
+srun --gres=gpu:1 --time=72:00:00 --pty bash
 
+# --qos=interactive
 # --gres=1 --qos=normal
 
 # Activate the conda environment
