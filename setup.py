ebcc0f3530034a0c267e17a25d63fcf5f335adb7
ynowatzk <ch4perone@gmail.com>
2025-10-16 15:46:20 +0200

    Update setup script and add .toml file to make development more future proof

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
+from pathlib import Path
 from setuptools import setup
 
-setup(name='fiora',
-      version='0.0.1',
-      long_description='file: README.md',
-      author='Yannek Nowatzky',
-      license='MIT',
-      classifiers=[
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3',
-      ],
+README = Path(__file__).with_name("README.md").read_text(encoding="utf-8")
 
-      scripts=["scripts/fiora-predict"],
-      packages=['fiora', 'fiora.GNN', 'fiora.IO',  'fiora.MOL', 'fiora.MS', 'fiora.visualization', 'models'],
-      include_package_data=True,
-      package_data={
-        'models': ['fiora_OS_v0.1.0.pt', 'fiora_OS_v0.1.0_state.pt', 'fiora_OS_v0.1.0_params.json'],
-      },
-      install_requires=['numpy', 'seaborn', 'torch', 'torch_geometric', 'dill', 'rdkit', 'treelib', 'spectrum_utils', 'setuptools>=24.2.0'],
-      python_requires='>=3.10.8',
-      # Developers may also want to install: jupyter torchmetrics umap umap-learn pytest
-      )
+setup(
+    name='fiora',
+    version='1.0.1',
+    description='In silico fragmentation and MS/MS simulation',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Yannek Nowatzky',
+    license='MIT',
+    classifiers=[
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    scripts=["scripts/fiora-predict"],
+    packages=['fiora', 'fiora.GNN', 'fiora.IO', 'fiora.MOL', 'fiora.MS', 'fiora.visualization', 'models'],
+    include_package_data=True,
+    package_data={
+        'models': [
+            'fiora_OS_v0.1.0.pt',
+            'fiora_OS_v0.1.0_state.pt',
+            'fiora_OS_v0.1.0_params.json',
+            'fiora_OS_v1.0.0.pt',
+            'fiora_OS_v1.0.0_state.pt',
+            'fiora_OS_v1.0.0_params.json',
+        ],
+    },
+    install_requires=['numpy', 'seaborn', 'torch', 'torch_geometric', 'dill', 'rdkit', 'treelib', 'spectrum_utils', 'ipython>=8'],
+    python_requires='>=3.10.8',
+)
